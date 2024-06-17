62a6f7551022f7b282d8639bf4308326ef1bf6de
Danail73 <dani.marinov.dm@gmail.com>
2024-06-17 14:33:35 +0300

    user controllers

---
+++ Yu-Gi-Oh-BackEnd/Controllers/FetchUserInfo.cs
@@ -0,0 +1,29 @@
+﻿using BusinessLayer;
+using DataLayer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Yu_Gi_Oh_BackEnd.Controllers
+{
+    [ApiController]
+    [Route("/hi")]
+    public class FetchUserInfoController : Controller
+    {
+        YuGiOhDbContext _yugiohContext = new YuGiOhDbContext();
+        [HttpGet(Name = "getuser")]
+        public Object GetUserInfo([FromBody] string username)
+        {
+            User[] users = _yugiohContext.Users.ToArray();
+            var result =
+                from user in users
+                where user.UserName == username
+                select new 
+                {
+                    user.Id,
+                    user.UserName,
+                    user.Password,
+                    Decks = user.Decks.ToArray() 
+                };
+            return result;
+        }
+    }
+}
---
+++ Yu-Gi-Oh-BackEnd/Controllers/FetchUsers.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer;
+using DataLayer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Yu_Gi_Oh_BackEnd.Controllers
+{
+    [ApiController]
+    [Route("getloginusers")]
+    public class FetchUsersController : Controller
+    {
+        YuGiOhDbContext _yugiohContext = new YuGiOhDbContext();
+        [HttpGet(Name = "getloginusers")]
+        public IEnumerable<Object> GetAllUsers()
+        {
+            //User[] users = _yugiohContext.Users.ToArray();
+            var result =
+                from user in _yugiohContext.Users
+                select new { user.UserName, user.Password };
+            return result;
+        }
+    }
+}
